14b02eec5ac572626d5b31654a21d986ce0428b2
Anish jha <122715402+anish-stack@users.noreply.github.com>
2025-01-27 13:30:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,8 +27,8 @@ ConnectDb();
 
 // Middlewares
 app.use(cookieParser());
-app.use(express.json({ limit: '1gb' }));
-app.use(express.urlencoded({ extended: true, limit: '1gb' }));
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ extended: true, limit: '500mb' }));
   app.use(morgan('combined'));
 app.use(cors());
 
