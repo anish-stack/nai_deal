7205c3a704bdec27ff7e1603f84aa827479058bf
Anish jha <122715402+anish-stack@users.noreply.github.com>
2025-01-27 13:34:32 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ ConnectDb();
 app.use(cookieParser());
 app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ extended: true, limit: '500mb' }));
-  app.use(morgan('combined'));
+  app.use(morgan('common'));
 app.use(cors());
 
 // Health check route
